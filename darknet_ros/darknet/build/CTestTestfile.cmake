f007a57dbac4420f55ec4ea46657a3f8c7ff2de9
mookie <mookie1702@gmail.com>
2020-12-09 12:58:37 +0800

    “First-commit”

---
+++ darknet_ros/darknet/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mooke2/catkin_workspace/src/darknet_ros/darknet/src
+# Build directory: /home/mooke2/catkin_workspace/src/darknet_ros/darknet/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ darknet_ros/darknet/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mooke2/catkin_workspace/src/darknet_ros/darknet/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/mooke2/catkin_workspace/devel;/opt/ros/kinetic'.split(';') if '/home/mooke2/catkin_workspace/devel;/opt/ros/kinetic' != '' else []
